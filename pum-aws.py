6332c346e037b9a7cf289f053af44df1cbdcf331
Alban Fonrouge <alban.fonrouge@visma.com>
2018-08-13 14:24:06 +0200

    * --help, arguments support * Add support for AWS profiles in credentials and config file (-p, --profile) * Filter roles based on AWS account number (-a, --account) * Remember last used user name in ~/.pum-aws * Specify region on command line (-r, --region)

--- pum-aws.py
+++ pum-aws.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 
 import sys
@@ -10,24 +10,57 @@ import base64
 import xml.etree.ElementTree as ET
 import re
 import os
+import argparse
 from bs4 import BeautifulSoup
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
 
 # Variables
-profile_region = 'eu-west-1'
 profile_output = 'json'
 sslverification = True
 idpentryurl = 'https://federation.visma.com/adfs/ls/idpinitiatedsignon.aspx?loginToRp=urn:amazon:webservices'
 tokenDuration = 60*60
 credentials_path = os.path.join(os.path.expanduser("~"), ".aws", "credentials")
 config_path = os.path.join(os.path.expanduser("~"), ".aws", "config")
+pumaws_configpath = os.path.join(os.path.expanduser("~"), ".pum-aws")
+
+parser = argparse.ArgumentParser(description="Get temporary AWS credentials using Visma federated access with privileged users.")
+parser.add_argument("-p", "--profile", default="default", help="Store credentials for a non-default AWS profile (default: override default credentials)")
+parser.add_argument("-a", "--account", help="Filter roles for the given AWS account")
+parser.add_argument("-r", "--region", help="Configure profile for the specified AWS region (default: eu-west-1)", default="eu-west-1")
+
+args = parser.parse_args()
+
+section=args.profile
+account=args.account
+profile_region = args.region
+
+# Read last used user name
+pumaws_config = configparser.RawConfigParser()
+pumaws_config.read(pumaws_configpath)
+lastuser = ""
+if pumaws_config.has_section("default"):
+    lastuser = pumaws_config.get("default", "username")
 
 # Get the federated credentials from the user
-print("Warning: This script will overwrite your default AWS credentials stored at "+credentials_path+"\n")
-print("Privileged user (e.g. adm\dev_aly):", end=" ")
-username = input()
+print("Warning: This script will overwrite your AWS credentials stored at "+credentials_path+", section ["+section+"]\n")
+if lastuser != "":
+    username = input("Privileged user (e.g. adm\dev_aly) [" + lastuser + "]: ")
+else:
+    username = input("Privileged user (e.g. adm\dev_aly): ")
+
+if username == "":
+    username = lastuser
+
 password = getpass.getpass(prompt='Domain password: ')
+
+# Save last used user name
+if lastuser != username and username is not None and username != "":
+    if not pumaws_config.has_section("default"):
+        pumaws_config.add_section("default")
+    pumaws_config.set("default", 'username', username)
+    with open(pumaws_configpath, 'w') as configfile:
+        pumaws_config.write(configfile)
 
 # 1st HTTP request: GET the login form
 session = requests.Session()
@@ -99,6 +132,10 @@ for awsrole in awsroles:
         awsroles.insert(index, newawsrole)
         awsroles.remove(awsrole)
 
+## Filter roles based on the specified account
+if account is not None:
+    awsroles = list(filter(lambda x: account in x, awsroles))
+
 # If user has more than one role, ask the user which one they want, otherwise just proceed
 print("")
 if len(awsroles) > 1:
@@ -128,22 +165,27 @@ token = client.assume_role_with_saml(RoleArn = role_arn, PrincipalArn = principa
 # Write the AWS STS token into the AWS credential file
 credentials_config = configparser.RawConfigParser()
 credentials_config.read(credentials_path)
-if not credentials_config.has_section('default'):
-    credentials_config.add_section('default')
-credentials_config.set('default', 'aws_access_key_id', token['Credentials']['AccessKeyId'])
-credentials_config.set('default', 'aws_secret_access_key', token['Credentials']['SecretAccessKey'])
-credentials_config.set('default', 'aws_session_token', token['Credentials']['SessionToken'])
+if not credentials_config.has_section(section):
+    credentials_config.add_section(section)
+credentials_config.set(section, 'aws_access_key_id', token['Credentials']['AccessKeyId'])
+credentials_config.set(section, 'aws_secret_access_key', token['Credentials']['SecretAccessKey'])
+credentials_config.set(section, 'aws_session_token', token['Credentials']['SessionToken'])
 os.makedirs(os.path.dirname(credentials_path), exist_ok=True)
 with open(credentials_path, 'w') as configfile:
     credentials_config.write(configfile)
-	
+
 # Write the AWS config file
+if section != "default":
+    config_section="profile " + section
+else:
+    config_section=section
+
 config_config = configparser.RawConfigParser()
 config_config.read(config_path)
-if not config_config.has_section('default'):
-    config_config.add_section('default')
-config_config.set('default', 'region', profile_region)
-config_config.set('default', 'output', profile_output)
+if not config_config.has_section(config_section):
+    config_config.add_section(config_section)
+config_config.set(config_section, 'region', profile_region)
+config_config.set(config_section, 'output', profile_output)
 os.makedirs(os.path.dirname(config_path), exist_ok=True)
 with open(config_path, 'w') as configfile:
     config_config.write(configfile)
@@ -153,5 +195,4 @@ with open(config_path, 'w') as configfile:
 print('\n----------------------------------------------------------------')
 print('Your AWS access key pair has been stored in the AWS configuration file {0}'.format(credentials_path))
 print('Note that it will expire at {0}'.format(token['Credentials']['Expiration']))
-print('Usage example: aws s3api list-buckets') #--profile default
 print('----------------------------------------------------------------\n')
