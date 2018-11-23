966e215b4a0fd2b374088a61a5dff486cb68228a
Francisco Javier Campa Lus <francisco.j.c.lus@visma.com>
2018-11-23 17:38:40 +0100

    Initial commit. Adding dockerfile, .bat and .sh to execute pum-aws without python.

---
+++ pum-aws.bat
@@ -0,0 +1,5 @@
+@echo off
+IF NOT EXIST "%USERPROFILE%\.aws" md %USERPROFILE%\.aws
+IF NOT EXIST "%USERPROFILE%\.pum-aws" type nul > %USERPROFILE%\.pum-aws
+docker build -t pum-aws .
+docker run --rm -it -v %USERPROFILE%\.aws:/root/.aws -v %USERPROFILE%\.pum-aws:/root/.pum-aws pum-aws
